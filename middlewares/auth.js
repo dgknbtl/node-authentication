2076a46730b47b5028c45ee1681856b0d4bafdea
Doğukan Batal <dogukanbatal@gmail.com>
2022-05-07 18:28:34 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
-   ensureAuthenticated: function (req, res, next) {
+   ensureAuthenticated(req, res, next) {
       if (req.isAuthenticated()) {
          return next()
       }
       req.flash('error_message', 'Please log in!')
       res.redirect(303, '/users/login')
    },
-   forwardAuthenticated: function (req, res, next) {
+   forwardAuthenticated(req, res, next) {
       if (!req.isAuthenticated()) {
          return next()
       }
